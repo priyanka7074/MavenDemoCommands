d5636e5f90aa0fd18635428528da6e9765ab216a
priyanka7074 <priyanka.katarey@gmail.com>
2019-06-13 17:14:18 -0400

    Maven CMD line codes example

---
+++ src/test/java/com/qa/tests/DemoTest1.java
@@ -0,0 +1,43 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+	@Test
+	public void sum() {
+		System.out.println("SUM");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(a+b, 30);
+		
+	}
+	
+	@Test
+	public void sub() {
+		System.out.println("SUB");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(b-a, 10);
+		
+	}
+	
+	@Test
+	public void div() {
+		System.out.println("DIV");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(b/a, 2);
+		
+	}
+	
+	@Test
+	public void mul() {
+		System.out.println("MUL");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(a*b, 200);
+		
+	}
+
+}
